a8e9bcb45cab1181f4ad3558493d913731f63918
tavolafourcade <tavolafourcade@gmail.com>
2022-12-08 10:13:13 -0500

    [ ADD ] Login with email

---
+++ src/app/signinForm.js
@@ -0,0 +1,31 @@
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { auth } from './firebase.js'
+import { showMessage } from "./showMessage.js";
+
+const signInForm = document.querySelector('#signin-form');
+
+signInForm.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const signinEmail = signInForm['signin-email'].value;
+  const signinPassword = signInForm['signin-password'].value;
+
+  try {
+    const credentials = await signInWithEmailAndPassword(auth, signinEmail, signinPassword)
+    console.log('credentials',credentials)
+
+    // Close the signin modal
+    const signupModal = document.querySelector('#signinModal')
+    const modal = bootstrap.Modal.getInstance(signupModal)
+    modal.hide()
+
+    showMessage(`You have successfully signed in ${credentials.user.email}`)
+  } catch (error) {
+    if (error.code === 'auth/wrong-password'){
+      showMessage('Wrong password', "error")
+    } else if (error.code === 'auth/user-not-found'){
+      showMessage('User not found', "error")
+    } else {
+      showMessage(error.message, "error")
+    }
+  }
+})
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/fi
 import { auth } from './app/firebase.js'
 import { loginCheck } from './app/loginCheck.js'
 import './app/signupForm.js'
+import './app/signinForm.js'
 import './app/logout.js'
 
 onAuthStateChanged(auth, async (user) => {
